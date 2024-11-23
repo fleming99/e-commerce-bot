2c9852a250cb5f2e115b6e8ccbe2e7659ac98c52
fleming99 <rfleming1235@gmail.com>
2024-11-23 19:56:53 -0300

    Initial files of the project.

---
+++ src/main/java/com/devfleming/e_commerce_bot/ECommerceBotApplication.java
@@ -0,0 +1,13 @@
+package com.devfleming.e_commerce_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfleming/e_commerce_bot/ECommerceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfleming.e_commerce_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
