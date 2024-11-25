1d19513cdaaccbd18c36b0875c0abcbbffe684c6
fleming99 <rfleming1235@gmail.com>
2024-11-24 23:41:02 -0300

    Adicionado a entidade Product que será utilizada para salvar os produtos.

---
+++ src/main/java/com/devfleming/e_commerce_bot/domain/entities/Product.java
@@ -0,0 +1,30 @@
+package com.devfleming.e_commerce_bot.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id", unique = true, nullable = false)
+    private Long productId;
+
+    @Column(name = "product_name", length = 50, nullable = false)
+    private String productName;
+
+    @Column(name = "product_description", length = 100, nullable = false)
+    private String productDescription;
+
+    @Column(name = "product_type", length = 50, nullable = false)
+    private String productType;
+}
